dc61e24ca24b7fb55053490afddc3f107154b1fd
Ece Aksu <115068334+eceaaksu@users.noreply.github.com>
2024-08-13 14:08:56 +0300

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,33 +1,28 @@
-import React from 'react';
-import Logo from '../assets/img/logo-white.svg';
-import { social } from '../data';
+import React from "react";
+import Logo from "../assets/img/logo-white.svg";
+import { social } from "../data";
 
 const Footer = () => {
   return (
-   <footer className='bg-orange py-8 lg:py-4'>
-    <div className='container mx-auto'>
-      <div className='flex text-white flex-col gap-y-6 lg:flex-row items-center justify-between'>
-        {/* logo */}
-        <a href='#'>
-          <img src= {Logo} alt='' />
-          </a>
-          {/* copyright text */}
-        <div className='text-[15px]'>
-          &copy Copyright 2024. All rights reserved.
-        </div>
-        {/* socials */}
-        <div className='flex-gap-x-4 '>
-          {social.map((item, index)=> {
-            return (
-              <a href={item.href} key={index}>
-                <div className='bg-orange-secondary hover:bg-transparent w-10 h-10 rounded-full flex justify-center items-center transition text-2xl'> {item.icon}</div>
-              </a>
-            );
-          })}
+    <footer className="bg-orange py-8 lg:pu-4">
+      <div className="container mx-auto">
+        <div className="flex text-white flex-col gap-y-6 lg:flex-row items-center justify-between">
+          <img src={Logo} alt="logo" />
+          <div className="text-[15px]">&copy; 2022 All rights reserved</div>
+          <div className="flex gap-x-4">
+            {social.map((item, index) => {
+              return (
+                <a key={index} href={item.href}>
+                  <div className="bg-[#fe8d71a9] hover:bg-[#fe8d71] w-10 h-10 rounded-full flex justify-center items-center transition text-2xl ">
+                    {item.icon}
+                  </div>
+                </a>
+              );
+            })}
+          </div>
         </div>
       </div>
-    </div>
-   </footer>
+    </footer>
   );
 };
 
